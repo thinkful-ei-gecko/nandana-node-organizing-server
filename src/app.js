d85f0ac67522a5bff382d125bf6428bac1966310
nrrao <nandanarao@yahoo.com>
2019-09-27 12:21:11 -0400

    finished bookmark router

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const logger = require('./logger');
 
+const bookmarkRouter = require('./bookmarks');
 const app = express();
 
 const morganOption = (NODE_ENV === 'production')
@@ -14,6 +16,23 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+app.use(express.json());
+
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  // move to the next middleware
+  next();
+});
+
+app.use(bookmarkRouter);
+
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
---
+++ src/store.js
@@ -0,0 +1,18 @@
+const bookmarks=[{
+  id:'457038c1-22d9-4eb8-8171-21493b69c164',
+  title:'yahoo',
+  url:'https://www.yahoo.com',
+  description:'yahoo',
+  rating:4
+
+},
+{
+  id:'21c90fed-2860-4934-894f-1b12f098eb2a',
+  title:'google',
+  url:'https://www.google.com',
+  description:'search engine',
+  rating:5
+
+}];
+
+module.exports={bookmarks};
