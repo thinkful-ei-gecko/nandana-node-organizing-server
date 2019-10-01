cbe6778d6d14561ab6de20f614c742df829ecf44
nrrao <nandanarao@yahoo.com>
2019-09-30 20:26:03 -0400

    organized error handling

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
-const logger = require('./logger');
-
+const validateBearerToken = require('./validateBearerToken');
+const errorHandler = require('./errorHandler');
 const bookmarkRouter = require('./bookmarks');
 const app = express();
 
@@ -17,19 +17,9 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(validateBearerToken);
 
 
-app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN;
-  const authToken = req.get('Authorization');
-
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
-    logger.error(`Unauthorized request to path: ${req.path}`);
-    return res.status(401).json({ error: 'Unauthorized request' });
-  }
-  // move to the next middleware
-  next();
-});
 
 app.use(bookmarkRouter);
 
@@ -38,15 +28,5 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
-});
-
+app.use(errorHandler);
 module.exports = app;
